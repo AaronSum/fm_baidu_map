a267451cf017f6106e06cf8fd1940b32a10c6ae2
孙荣忠 <rzsun@86links.com>
2020-05-08 16:24:27 +0800

    #

--- android/src/main/java/com/hhwy/fm_baidu_map/FmBaiduMapView.java
+++ android/src/main/java/com/hhwy/fm_baidu_map/FmBaiduMapView.java
@@ -1,6 +1,7 @@
 package com.hhwy.fm_baidu_map;
 
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.CircleOptions;
 import com.baidu.mapapi.map.MapStatus;
@@ -175,7 +176,9 @@ public class FmBaiduMapView{
     FmBaiduMapView(String name,PluginRegistry.Registrar registrar, FmBaiduMapViewFactory factory){
         _registrar = registrar;
         _ftb = new FmToolsBase(this, name, registrar);
-        _view=new TextureMapView(registrar.activity());
+         BaiduMapOptions options = new BaiduMapOptions();
+         options.overlookingGesturesEnabled(false);
+        _view = new TextureMapView(registrar.activity(),options);
         _bmp = _view.getMap();
         _bmp.setMyLocationEnabled(true);
         _factory = factory;
